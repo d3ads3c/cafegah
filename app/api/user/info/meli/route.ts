fbb03a911e308641c12dc84066bef27e562d32c3
owl-server <numdar.r2@gmail.com>
2025-12-19 06:31:04 -0500

    V 1.15.1

--- app/api/user/info/meli/route.ts
+++ app/api/user/info/meli/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getClientIp } from "../../_utils";
+import { getClientIp } from "../../../_utils";
 
 export async function POST(req: NextRequest) {
     try {
