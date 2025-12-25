1d80adb08d61075c683cdb967ee54693e5ef6663
owl-server <numdar.r2@gmail.com>
2025-12-25 22:01:18 +0330

    V 1.15.82

--- app/api/subscriptions/enter/route.ts
+++ app/api/subscriptions/enter/route.ts
@@ -41,7 +41,7 @@ formData.append("ipaddress", "127.0.0.1");
     if (backendData.Status === "Success") {
       return NextResponse.json(
         {
-          Link: "https://app.cafegah.ir",
+          Link: "http://10.77.119.13:3255",
           Status: "Success",
         },
         { status: 200 }
